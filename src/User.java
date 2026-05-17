05edb1205aecbb01249f21ed8b69ecce6e45ad3a
xEzIxX <by1094@gachon.ac.kr>
2026-05-17 16:34:13 +0900

    feat: User 클래스 추가

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public abstract class User {
+
+    private String name;
+
+    public User (String name){
+        this.name = name;
+    }
+
+    public String getName() {
+         return this.name;
+     }
+
+
+    public void showInfo() {
+        System.out.println("[유저 정보] 이름: " + name);
+    }
+}
