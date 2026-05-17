fb7996b0eb62def98567b5e1e3d7bc4df90b7634
xEzIxX <by1094@gachon.ac.kr>
2026-05-17 16:35:13 +0900

    feat: IBWorker 클래스 추가

---
+++ src/IBWorker.java
@@ -0,0 +1,31 @@
+import java.util.Map;
+
+public class IBWorker extends Worker {
+    public static final Map<String, Integer> recommendBoxList
+            = Map.of(
+            "phoneCase", 1,
+            "cup", 2,
+            "snack", 3
+    );
+
+    public IBWorker(String name, int assignedCount){
+        super(name, assignedCount);
+    }
+
+    public int getRecommendBoxNumber(String productName) {
+        if (!recommendBoxList.containsKey(productName)){
+            throw new IllegalArgumentException("포장할 수 없습니다.");
+        }
+        return recommendBoxList.get(productName);
+    }
+
+    public boolean completePackaging(String productName, int boxNumber) {
+        if (!recommendBoxList.containsKey(productName)){
+            throw new IllegalArgumentException("포장할 수 없습니다.");
+        }
+        if (recommendBoxList.get(productName) == boxNumber){
+            return true;
+        }
+        return false;
+    }
+}
