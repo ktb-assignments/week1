a317392133d3ee5983f022c81d7a4ae4836db416
xEzIxX <by1094@gachon.ac.kr>
2026-05-17 16:36:51 +0900

    feat: 메뉴 안내 클래스 Menu 추가

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+
+    public static void showCommonMenu() {
+        System.out.println("0. 도망치기");
+        System.out.println("1. 작업자 정보 보기");
+        System.out.println("2. 작업 더 할당하기");
+        System.out.println("3. 퇴근 하기");
+    }
+
+    public static void shoIBWorkerMenu() {
+        System.out.println("11. 포장 박스 호수 추천받기");
+        System.out.println("12. 포장하기");
+    }
+}
